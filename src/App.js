94addc813158e359789476a2a6126e825d34ce00
weilyuwang <weilyuwang@gmail.com>
2020-04-27 15:04:22 -0400

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import produce from "immer";
+import { Button, Container } from "@material-ui/core";
 
 import "./App.css";
 
-const NUM_ROWS = 30;
+const NUM_ROWS = 28;
 const NUM_COLS = 40;
 
 const App = () => {
@@ -18,34 +19,41 @@ const App = () => {
 
     console.log(grid);
     return (
-        <div
-            style={{
-                display: "grid",
-                margin: "40px 40px",
-                gridTemplateColumns: `repeat(${NUM_COLS}, 30px)`,
-                gridTemplateRows: `repeat(${NUM_ROWS}, 30px)`,
-            }}
-        >
-            {grid.map((rows, i) =>
-                rows.map((col, j) => (
-                    <div
-                        key={`${i}-${j}`}
-                        onClick={() => {
-                            const newGrid = produce(grid, (gridCopy) => {
-                                gridCopy[i][j] = 1;
-                            });
-                            setGrid(newGrid);
-                        }}
-                        style={{
-                            width: 25,
-                            height: 25,
-                            backgroundColor: grid[i][j] ? "pink" : undefined,
-                            border: "solid 1px #4d5259",
-                        }}
-                    />
-                ))
-            )}
-        </div>
+        <Container style={{ marginTop: "20px" }}>
+            <Button variant="contained" color="primary">
+                Start
+            </Button>
+            <div
+                style={{
+                    marginTop: "20px",
+                    display: "grid",
+                    gridTemplateColumns: `repeat(${NUM_COLS}, 30px)`,
+                    gridTemplateRows: `repeat(${NUM_ROWS}, 30px)`,
+                }}
+            >
+                {grid.map((rows, i) =>
+                    rows.map((col, j) => (
+                        <div
+                            key={`${i}-${j}`}
+                            onClick={() => {
+                                const newGrid = produce(grid, (gridCopy) => {
+                                    gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                                });
+                                setGrid(newGrid);
+                            }}
+                            style={{
+                                width: 25,
+                                height: 25,
+                                backgroundColor: grid[i][j]
+                                    ? "pink"
+                                    : undefined,
+                                border: "solid 1px #4d5259",
+                            }}
+                        />
+                    ))
+                )}
+            </div>
+        </Container>
     );
 };
 
